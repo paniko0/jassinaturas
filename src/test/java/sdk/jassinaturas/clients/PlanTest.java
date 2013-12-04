69be4efc34c21885abaf42bb4a2ccebc09d4e0e8
Danillo Souza <danillo.fs@gmail.com>
2013-12-04 00:10:36 -0200

    innitial commit

---
+++ src/test/java/sdk/jassinaturas/clients/PlanTest.java
@@ -0,0 +1,26 @@
+package sdk.jassinaturas.clients;
+
+import org.junit.Test;
+
+public class PlanTest {
+
+	@Test
+	public void shouldCreateANewPlan() {
+
+	}
+
+	@Test
+	public void shouldUpdateAPlan() {
+
+	}
+
+	@Test
+	public void shouldListAllPlans() {
+
+	}
+
+	@Test
+	public void shouldShowAPlan() {
+
+	}
+}

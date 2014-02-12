c310fa5952f42daa83e28964cf6b0ce409648c36
Danillo Souza <danillo.fs@gmail.com>
2014-02-11 22:31:18 -0200

    adding missing files

---
+++ src/main/java/sdk/jassinaturas/clients/attributes/CreationDate.java
@@ -0,0 +1,60 @@
+package sdk.jassinaturas.clients.attributes;
+
+
+public class CreationDate {
+    private int day;
+    private int hour;
+    private int minute;
+    private int month;
+    private int second;
+    private int year;
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setDay(final int day) {
+        this.day = day;
+    }
+
+    public void setHour(final int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(final int minute) {
+        this.minute = minute;
+    }
+
+    public void setMonth(final int month) {
+        this.month = month;
+    }
+
+    public void setSecond(final int second) {
+        this.second = second;
+    }
+
+    public void setYear(final int year) {
+        this.year = year;
+    }
+
+}
---
+++ src/main/java/sdk/jassinaturas/clients/attributes/ExpirationDate.java
@@ -0,0 +1,32 @@
+package sdk.jassinaturas.clients.attributes;
+
+public class ExpirationDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setDay(final int day) {
+        this.day = day;
+    }
+
+    public void setMonth(final int month) {
+        this.month = month;
+    }
+
+    public void setYear(final int year) {
+        this.year = year;
+    }
+
+}

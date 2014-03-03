2367572ab1ce5335e55b4dc5a49af8d8eda37c1a
Danillo Souza <danillo.fs@gmail.com>
2014-03-03 19:25:06 -0300

    adding toString on Alerts class

--- src/main/java/sdk/jassinaturas/clients/attributes/Alerts.java
+++ src/main/java/sdk/jassinaturas/clients/attributes/Alerts.java
@@ -1,15 +1,21 @@
 package sdk.jassinaturas.clients.attributes;
 
 public class Alerts {
-	
-	private String description;
-	private String code;
-	
-	public String getCode() {
-		return code;
-	}
-	
-	public String getDescription() {
-		return description;
-	}
+
+    private String description;
+    private String code;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public String toString() {
+        return "Alerts [description=" + description + ", code=" + code + "]";
+    }
+
 }

86ee55d80558204b7f2701f58e49bbe2e09e7b1b
Danillo Souza <danillo.fs@gmail.com>
2014-03-05 20:42:09 +0000

    validating when error body is null

--- src/main/java/sdk/jassinaturas/communicators/ErrorHandler.java
+++ src/main/java/sdk/jassinaturas/communicators/ErrorHandler.java
@@ -13,6 +13,13 @@ public class ErrorHandler implements ErrorDecoder {
 
     @Override
     public Exception decode(final String arg0, final Response arg1) {
+      
+      if(arg1.body() == null){
+        String json = "{\"message\":\"Http Status 400 happened\", \"errors\": [{\"code\": \"UNKNOWN\", \"description\": \"Unknown error\"}]}";
+        GsonDeserializer gson = new GsonDeserializer();
+        ApiResponseError error = gson.deserialize(json, ApiResponseError.class);
+        throw new ApiResponseErrorException(error);
+      }
 
         try {
             InputStream is = arg1.body().asInputStream();
@@ -24,7 +31,7 @@ public class ErrorHandler implements ErrorDecoder {
         } catch (IOException e) {
             System.out.println("Error when parsin response" + e);
         }
-
-        return new ApiResponseErrorException("Http Status 400 happened");
+      
+      return new ApiResponseErrorException("Http Status 400 happened");
     }
 }
